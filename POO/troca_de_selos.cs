996eed5d257a63a27ffe7d263a62e714ee0c44a7
Shen <tomasstocker1@gmail.com>
2022-10-27 15:20:51 +0100

    Add project files.

---
+++ POO/troca_de_selos.cs
@@ -0,0 +1,89 @@
+
+namespace POO
+{
+    class Selos
+    {
+        public Selos (int euros)
+        {
+            string valor;
+            int s5, s3, trouco;
+
+            if (euros < 8) Console.WriteLine("Valor inválido, mínimo 8€!");
+            else 
+            {
+                Console.WriteLine("Quantos selos de 3€?");
+                valor = Console.ReadLine();
+                s3 = int.Parse(valor);
+                Console.WriteLine("Quantos selos de 5€?");
+                valor = Console.ReadLine();
+                s5 = int.Parse(valor);
+                trouco = Verifica(euros, s3, s5);
+
+                switch(trouco) 
+                {
+                    case -1:
+                        Console.WriteLine("Valor inválido, núm. de selos não pode ser zero!");
+                        break;
+                    case 0:
+                        Console.WriteLine("Valor inválido, núm. de selos excede o valor introduzido!");
+                        break;
+                    default:
+                        Console.WriteLine("########################");
+                        Console.WriteLine("Com {0} euros comprou: ", euros);
+                        Console.WriteLine(" - {0} selos de 3 euros", s3);
+                        Console.WriteLine(" - {0} selos de 5 euros", s5);
+                        Console.WriteLine("Perdeu {0} de trouco", trouco);
+                        Console.WriteLine("########################");
+                        break;
+                }
+            }
+        }
+
+        public int Verifica (int valor, int num_selos3, int num_selos5)
+        {
+            int resto = 0;
+
+            if ( num_selos3 <= 0 && num_selos5 <= 0) return -1; // erro de inputs
+            
+            else if ( num_selos3 == 0 && num_selos5 > 0)
+            {
+                if ( valor >= num_selos5 * 5)
+                {
+                    resto = valor - (5 * num_selos5);
+                    return resto;
+                }
+                else return 0; // numero de selos superior aos euros 
+            }
+            
+            else if ( num_selos3 > 0 && num_selos5 == 0)
+            {
+                if ( valor >= num_selos3 * 3)
+                {
+                    resto = valor - (3 * num_selos3);
+                    return resto;
+                }
+                else return 0;
+            }
+
+            else if ( num_selos3 > 0 && num_selos5 > 0)
+            {
+                 if ( valor >= (num_selos3 * 3) + (num_selos5 * 5))
+                {
+                    resto = valor - ((3 * num_selos3) + (num_selos5 * 5)) ;
+                    return resto;
+                }
+                else return 0;
+            }
+            else return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.WriteLine("Introduza o seu dinheiro: ");
+            valor = int.Parse(Console.ReadLine());
+            Selos var = new Selos(valor);
+        }
+    }
+}
---
+++ ficha1/ficha1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+public class Ficha1
+{
+    public void Dobro(float num)
+    {
+		//while (!float.TryParse(Console.ReadLine(), out num)) Console.Write("Não é um número! Volte a tentar: ");
+		Console.Write("Dobro de " + num);
+		num *= 2;
+        Console.WriteLine(" é " + num);
+    }
+    public void Par(float num)
+    {
+		//while (!float.TryParse(Console.ReadLine(), out num)) Console.Write("Não é um número! Volte a tentar: ");
+		if (num % 2 == 0) Console.WriteLine("Número " + num + " é par");
+        else Console.WriteLine("Número " + num + "não é par!");
+    }
+    public void Soma(int num)
+    {
+		//while (!int.TryParse(Console.ReadLine(), out num)) Console.Write("Não é um número! Volte a tentar: ");
+		int total = 0;
+        for (int i = 0; i < num; i++) total += i;
+        Console.WriteLine("Total= " + total);
+    }
+    public int Soma2()
+    {
+        int num, total = 0;
+
+		Console.WriteLine("Introduza números inteiros para somar.");
+		Console.WriteLine("Para terminar, introduza ´-1´.");
+		while (true)
+        {
+            Console.Write("Input: ");
+            while (!int.TryParse(Console.ReadLine(), out num)) Console.Write("Não é um número inteiro! Volte a tentar: ");
+            if (num != 0 && num == -1)
+            {
+                Console.WriteLine("Total da soma = {0}", total);
+                Console.Read();
+                return 0;
+            }
+            total += num;
+        }
+    }
+    public void Inverter1(int num)
+    {
+		/*char temp;
+		numI = num.ToString();
+		char temp = '\0';
+		string numI = num.ToString();
+		temp = numI[0];
+		numI[0] = numI[2];
+		numI[2] = temp;
+		 */
+		if (num >= 100 && num <= 999)
+        {
+			string numI = num.ToString();
+            char[] arr = numI.ToCharArray();
+            Array.Reverse(arr);
+            numI = arr.ToString();
+            num = int.Parse(arr);
+            Console.WriteLine(arr);
+			Console.Read();
+		}
+		else
+        {
+            Console.WriteLine("Input inválido!");
+            Console.Read();
+        }
+	}
+    static void Main(string[] args)
+    {
+        Ficha1 ficha1 = new();
+		ficha1.Dobro(8);
+		ficha1.Par(8);
+		ficha1.Soma(3);
+		ficha1.Soma2();
+        ficha1.Inverter1(100);
+    }
+}
