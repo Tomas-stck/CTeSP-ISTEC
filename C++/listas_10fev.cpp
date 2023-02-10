6485eed5d456515336ac7e1647377c1a4cbc6889
Your Name <tomasstoker1@gmail.com>
2023-02-10 17:25:56 +0000

    listas

--- C++/listas_10fev.cpp
+++ C++/listas_10fev.cpp
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 using namespace std;
 
+struct pes{
+
+	char	nome[81];
+	int		idade;
+	pes		*next; 		// pointer que aponta para o tipo 'pes'
+
+} *pHead = NULL; 		// aqui fica já declarado a var pointer pHead que aponta para structs 'pes'
+
+void	mostraMenu();
+
 int main(int argc, char** argv) {
 
 	//cabeça da lista é uma var estática
@@ -12,26 +22,79 @@ int main(int argc, char** argv) {
 	//Lista Simples (com TAIL) ################
 
 	//testar se a lista está vazia
-	 if(!pHead) então lista vazia
+	// if(!pHead) então lista vazia
 
 	//condição para testar se a lista tem apenas um elemento:
-	if (pHead == pTail && (!pHead && !pTail))
-
+	// if (pHead == pTail && (!pHead && !pTail))
 
 	//Lista Simples (sem TAIL) ################
 
 	//testar se está vazio
-	 if(!pHead)
+	// if(!pHead)
 
 	//testar se só tem 1 elemento
-	 if(!pHead->next)
+	// if(!pHead->next)
 
 	//ler o campo do proximo elemento
-	 pHead->next->campo
+	// pHead->next->campo
 
 	//Lista Bidireccional
 	//contém 2 pointers, ->next / ->prev
 
+	char	op;
 
+	do {
+		mostraMenu();
+		op = lerOpcao();
+		switch (op)
+		{
+		case 'i': //inserir
+			inserirHead();
+			break;
+		case 'a': // apagar
+			apagarHead();
+			break;
+		case 'l': // listar
+			listar();
+			break;
+		case 't': // terminar
+			cout << "Adeus!";
+			break;
+		default:
+			cout << "Opção inválida!\n";
+			break;
+		}
+	} while(op != 't');
+	return 0;
 }
 
+void	mostraMenu(){
+	cout << "\n 'i' - Inserir\n";
+	cout << "\n 'a' - Apagar\n";
+	cout << "\n 'l' - Listar\n";
+	cout << "\n 't' - Terminar\n";
+}
+
+bool	inserirHead(){
+	pes	*pNovo;
+
+	pNovo = (pes*) malloc (sizeof(pes));
+	if (!pNovo)
+		return false;
+	//pedir dados
+	cout<< "\nNome: ";
+	cin.clear();cin.sync();
+	cin.getline(pNovo->nome, 81);
+	cout<< "\nIdade: ";
+	cin.clear();cin.sync();
+	cin >> pNovo->idade;
+	//
+	//posicionar Pointers:
+	pNovo->next = pHead;
+	pHead = pNovo;
+	return true;
+}
+
+void	apagarHead(){
+
+}
