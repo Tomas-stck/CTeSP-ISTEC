d45c4414a09f53faacd849f88c463a5bd7a90c0d
Your Name <tomasstoker1@gmail.com>
2023-02-10 15:46:53 +0000

    listas

---
+++ C++/listas_10fev.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	//cabeça da lista é uma var estática
+	//cauda da lista também é estática.
+	//São ambas pointers.
+
+	//Lista Simples (com TAIL) ################
+
+	//testar se a lista está vazia
+	 if(!pHead) então lista vazia
+
+	//condição para testar se a lista tem apenas um elemento:
+	if (pHead == pTail && (!pHead && !pTail))
+
+
+	//Lista Simples (sem TAIL) ################
+
+	//testar se está vazio
+	 if(!pHead)
+
+	//testar se só tem 1 elemento
+	 if(!pHead->next)
+
+	//ler o campo do proximo elemento
+	 pHead->next->campo
+
+	//Lista Bidireccional
+	//contém 2 pointers, ->next / ->prev
+
+
+}
+
